05a955ac59a982252017b88be854dcd6451cba19
mbehboodi <mbehboodi6909@gmail.com>
2018-12-30 08:05:09 +0330

    add basic main functionality

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace dfa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("dfa.txt");
+            var dfa = new DFA();
+            int.TryParse(lines[2], out var tn);
+            int.TryParse(lines[1], out var sn);
+            var ts = new Dictionary<int, Dictionary<string, int>>(sn);
+            for (int i = 0; i < tn; i++)
+            {
+                var lineParts = lines[i + 3].Split(" ");
+                int.TryParse(lineParts[0], out var src);
+                int.TryParse(lineParts[2], out var dst);
+                if (ts.ContainsKey(src))
+                {
+                    ts.GetValueOrDefault(src).Add(lineParts[1], dst);
+                } else {
+                    ts.Add(src, new Dictionary<string, int> { { lineParts[1], dst } });
+                }
+            }
+
+            Log(ts);
+        }
+
+        static void Log(IDictionary<int, Dictionary<string, int>> d)
+        {
+            foreach (var item in d)
+            {
+                Console.WriteLine($"{item.Key}: ");
+                foreach (var entry in item.Value)
+                {
+                    Console.WriteLine($"    {entry.Key} : {entry.Value}");
+                }
+            }
+        }
+    }
+}
