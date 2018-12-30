43f2f4eab3364ef8e3914ae6b1f6ebf0f89235b2
mbehboodi <mbehboodi6909@gmail.com>
2018-12-30 10:39:19 +0330

    add builder javascript command

---
+++ build.js
@@ -0,0 +1,28 @@
+const { execSync } = require('child_process');
+
+const targets = [
+    "win7-x64",
+    "win7-x86",
+    "win8-x64",
+    "win8-x86",
+    "win81-x64",
+    "win81-x86",
+    "win10-x64",
+    "win10-x86",
+    "debian-x64",
+    "debian.9-x64",
+    "ubuntu-x64",
+    "ubuntu.16.04-x64",
+    "ubuntu.18.04-x64",
+    "linuxmint.17-x64",
+    "linuxmint.18-x64",
+    "linuxmint.18.1-x64",
+    "linuxmint.18.2-x64",
+    "linuxmint.18.3-x64"
+];
+
+for (const target of targets) {
+    console.log(`building ${target}...`);
+    execSync(`dotnet publish -c Release -r ${target}`);
+    console.log(`building ${target} completed.`)
+}
