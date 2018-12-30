7c5669d0431447e93b28cb1a2fb369201c643681
mbehboodi <mbehboodi6909@gmail.com>
2018-12-30 08:57:39 +0330

    add DFA ctor

--- DFA.cs
+++ DFA.cs
@@ -5,7 +5,14 @@ namespace dfa
 {
     class DFA
     {
-        public IList<IDictionary<string, int>> Transitions { get; set; }
+        public DFA(int tn, int sn, int initial, IList<int> acceptings)
+        {
+            Transitions = new Dictionary<int, IDictionary<string, int>>(tn);
+            States = sn;
+            Initial = initial;
+            Acceptings = new List<int>(acceptings);
+        }
+        public IDictionary<int, IDictionary<string, int>> Transitions { get; set; }
         public int Initial { get; set; }
         public IList<int> Acceptings { get; set; }
         public int States { get; set; }
